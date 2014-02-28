4ee9bbb1a78d2fb97a1f03508a35b2b3281b3207
Hein-Pieter van Braam <hp@tmm.cx>
2014-02-28 13:30:59 +0100

    Initial commit

---
+++ include/libudevmm.hpp
@@ -0,0 +1,8 @@
+#ifndef libudevmm_H
+#define libudevmm_H
+
+#include "stdio.h"
+
+extern void print_hello ();
+
+#endif
---
+++ libudevmm/libudevmm.cpp
@@ -0,0 +1,6 @@
+#include <libudevmm.hpp>
+
+void
+print_hello(){
+  printf("!!!Hello World!!!\n");
+}
