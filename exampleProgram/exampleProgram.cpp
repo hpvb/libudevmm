3e06213f804ad5480ec19d32efa28392c491f72e
Hein-Pieter van Braam <hp@tmm.cx>
2014-03-03 11:19:12 +0100

    License fixups

--- exampleProgram/exampleProgram.cpp
+++ exampleProgram/exampleProgram.cpp
@@ -1,16 +1,19 @@
-/*
- ============================================================================
- Name        : exampleProgram.cpp
- Author      : Hein-Pieter van Braam
- Version     :
- Copyright   : (c) 2014 Hein-Pieter van Braam
- Description : Uses shared library to print greeting
-               To run the resulting executable the LD_LIBRARY_PATH must be
-               set to ${project_loc}/libudevmm/.libs
-               Alternatively, libtool creates a wrapper shell script in the
-               build directory of this program which can be used to run it.
-               Here the script will be called exampleProgram.
- ============================================================================
+/* This file is part of libudevmm
+ *
+ * Copyright 2014 Hein-Pieter van Braam <hp@tmm.cx>
+ *
+ * libudevmm is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * libudevmm is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with systemd; If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <libudevmm.hpp>
